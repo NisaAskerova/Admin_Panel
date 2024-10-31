91c6106150f273b6bb054a1def77a726429ee1da
gulshanhasanzade <hasanzade678@gmail.com>
2024-10-31 20:25:11 +0400

    last commit

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,3 @@ Route::get('/', function () {
 // Route::post('/stores', [SliderController::class, 'stores'])->name('stores');
 // Route::get('/slider', [SliderController::class, 'show']);
 // });
-// düzəldi ) COX sag olun
-// daha hər dəfə githudan yükləməyin sadəcə burdan reloada basın son kodlar gələcək 
-
-// oldu
-
-// indi etmirəm çünkü iki dənə dəyişikliyiniz var onları göndərməsəz təzə kodlar çəkə bilməyəcksiz
-// gərək son kodları göndərək ki təzələri çəkə bilək  bir də çalışın ikiniz eyni faylda çox işləməyin conflick(çaxışma) olacaq yoxsa 
-// yəni ikində web.php faylında işləyəcəksizsə bacarın eyni sətirlərə kod yazmayın fərli sətirlər olsun yoxsa kodlar çaxışacaq ) tamam
-//başqa nəsə problem var?helekı yox 
-//tamamdır cox sag olun muəllim
-// buyurun)
